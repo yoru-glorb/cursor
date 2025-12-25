992980b2e51023d95a7dfd4e9c982a48541cfcc8
yoru-glorb <izbrannikb@gmail.com>
2025-12-25 21:29:50 +0300

    Add Header component

---
+++ Desktop/react-portfolio/react-portfolio/src/components/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Денис</h1>
+      <nav>
+        <ul>
+          <li><a href="">О нас</a></li>
+          <li><a href="">Портфолио</a></li>
+          <li><a href="">Махаури Черещ</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
